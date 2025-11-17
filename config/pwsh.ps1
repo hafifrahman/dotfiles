2aca3f08a63a7afa3675c921fed93b524a0f6507
Hafif Rahman <hazeha16@gmail.com>
2025-11-17 18:07:48 +0800

    refactor(pwsh): move config file

--- config/pwsh.ps1
+++ config/pwsh.ps1
@@ -1,12 +1,3 @@
-Import-Module DirColors
-
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider "Ctrl+f" -PSReadlineChordReverseHistory "Ctrl+r"
-
-Set-Alias vi nvim
-Set-Alias pn pnpm
-Set-Alias grep findstr
-
 function mkcd {
   param(
     [Parameter(Mandatory = $true)]
@@ -22,7 +13,5 @@ function which {
   (Get-Command -Name $Command -ErrorAction SilentlyContinue).Source
 }
 function path { $env:Path -split ";" }
-function la { Get-ChildItem -Force  }
+function la { Get-ChildItem -Force }
 function ll { Get-ChildItem -Force -Hidden }
-
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\robbyrussell.omp.json" | Invoke-Expression
